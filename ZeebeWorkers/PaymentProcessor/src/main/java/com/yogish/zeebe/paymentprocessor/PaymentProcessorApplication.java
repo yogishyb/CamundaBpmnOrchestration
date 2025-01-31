a5235f8b31e518830b5cac64f0fe21374604806c
yogishyb <yogishayb@gmail.com>
2025-01-31 15:34:09 +0530

    initial

---
+++ ZeebeWorkers/PaymentProcessor/src/main/java/com/yogish/zeebe/paymentprocessor/PaymentProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.yogish.zeebe.paymentprocessor;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentProcessorApplication.class,args);
+    }
+}
